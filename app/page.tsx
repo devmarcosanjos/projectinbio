b4fb96966868535dd9cb9649075801f61f90577c
marcos <dev.marcosanjos@gmail.com>
2025-02-04 10:29:31 -0300

    add Hero component and utility functions; update layout and page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import Hero from "@/app/components/landing-page/hero";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hello word</h1>;
+  return (
+    <div className="max-w-7xl mx-auto border">
+      <Hero />
+      {/* <Header />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ /> */}
+    </div>
+  );
 }
