272579989cc9b4d0197968c50de49e025f969454
marcos <dev.marcosanjos@gmail.com>
2025-02-05 13:51:07 -0300

    add authentication setup; integrate NextAuth with Firestore adapter and Google provider

---
+++ app/lib/auth.ts
@@ -0,0 +1,13 @@
+import { firebaseCert } from "@/app/lib/firebase";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: FirestoreAdapter({
+    credential: firebaseCert,
+  }),
+  providers: [Google], // Add your providers here
+  events: {},
+  callbacks: {},
+});
